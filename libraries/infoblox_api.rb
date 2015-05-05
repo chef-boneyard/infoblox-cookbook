d47ae8959eddc740e6723746220391fa03c99910
Prabhu <prabhu.das@clogeny.com>
2015-05-05 03:23:58 -0700

    Network workflow fixes

--- libraries/infoblox_api.rb
+++ libraries/infoblox_api.rb
@@ -3,7 +3,7 @@ module Infoblox
 
     require 'ipaddr'
     require 'infoblox'
-
+    require 'pry'
     # IP Validation.
     def is_valid_ip?(ipaddr)
       IPAddr.new(ipaddr) 
@@ -13,18 +13,17 @@ module Infoblox
 
     def request(usage_type, params = {})
       raise "No parameters defined." if params.empty?
-      send("create_#{usage_type}_record",params)
+      send("create_#{usage_type}_record", params)
     end
 
     def get_ip_address(params = {})
       raise "No parameters defined." if params.empty?
-      Chef::Log.info "get_ip_address"
-      record = Infoblox::Netowk.new(connection: connection, network: params[:network])
+      record = Infoblox::Network.new(connection: connection, network: params[:network])
       record.network_view = params[:network_view] if params[:network_view]
       record.network_container = params[:network_container] if params[:network_container]
       # TODO
       record.network = get_network_reference(record)
-      return record.next_available_ip
+      return record.next_available_ip.first
     end
 
     private 
@@ -35,10 +34,10 @@ module Infoblox
 
     def create_host_record(params)
       Chef::Log.info "create_host_record"
-      record = Infoblox::Arecord.new(connection: connection, 
-                                    ipv4addr: params[:ipv4addr],
-                                    name: params[:name],
-                                    view: params[:view])
+      record = Infoblox::Arecord.new(connection: connection)
+      record.ipv4addr = params[:ipv4addr] if params[:ipv4addr]
+      record.name = params[:name] if params[:name]
+      record.view = params[:view] if params[:view]
       begin
         record.post
         Chef::Log.info "Host Record is successfully created."
@@ -48,9 +47,10 @@ module Infoblox
     end
 
     def create_dns_record(params)
-      record = Infoblox::Host.new(connection: connection, 
-                                    ipv4addr: params[:ipv4addr],
-                                    name: params[:name])
+      record = Infoblox::Host.new(connection: connection)
+      record.ipv4addr = params[:ipv4addr] if params[:ipv4addr]
+      record.name = params[:name] if params[:name]
+      record.view = params[:view] if params[:view]
       begin
         record.post
         Chef::Log.info "DNS Record is successfully created."
@@ -60,10 +60,12 @@ module Infoblox
     end
 
     def create_fixed_address_record(params)
+      fixedaddr = Infoblox::Fixedaddress.new(connection: connection)
+      record.ipv4addr = params[:ipv4addr] if params[:ipv4addr]
+      record.name = params[:name] if params[:name]
+      record.view = params[:view] if params[:view]
+      record.mac = params[:mac_address] if params[:mac_address]
       begin
-        fixedaddr = Infoblox::Fixedaddress.new(connection: connection,
-                                            ipv4addr: params[:ipv4addr],
-                                            mac: params[:mac_address])
         fixedaddr.post
         Chef::Log.info "Fixed Address Record is successfully created."
       rescue Exception => e
@@ -72,7 +74,7 @@ module Infoblox
     end
 
     def get_network_reference(network)
-      JSON.parse(network.get.body).first["_ref"].gsub("network/")
+      JSON.parse(network.get.body).first["_ref"].gsub("network/", "")
     end
 
   end
--- providers/ip_address.rb
+++ providers/ip_address.rb
@@ -16,17 +16,20 @@ action :reserve_static_ip do
 end
 
 action :reserve_network_ip do
-  Chef::Log.info "Action : reserve_static_ip"
 
+  Chef::Log.info "Action : reserve_static_ip"
   # set request params.
   request_params = {}
   request_params[:network] = new_resource.network
+  request_params[:name] = new_resource.name
+  request_params[:view] = new_resource.view unless new_resource.view.nil? 
   request_params[:network_view] = new_resource.network_view unless new_resource.network_view.nil?
   request_params[:network_container] = new_resource.network_container unless new_resource.network_container.nil?
 
   # get next available ip address from defined network
-  request_params[:ipv4addr] = get_ip_address(params)
-
+  request_params[:ipv4addr] = get_ip_address(request_params)
+  Chef::Log.info "Next available IP in network is : #{request_params[:ipv4addr]}"
+  
   resp = request(new_resource.usage_type, request_params)
 
 end
