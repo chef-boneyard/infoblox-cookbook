9493c324cd6292f3243d5384ce631d007cbdc56e
Prabhu Das <prabhud@plumgrid.com>
2015-05-07 03:34:37 -0700

    Added implementation for cname and aaaa records.

--- resources/cname_record.rb
+++ resources/cname_record.rb
@@ -2,7 +2,7 @@
 actions :create, :get_record, :get_ip, :delete
 default_action :create
 
-attribute :name, kind_of: String, name_attribute: true
+attribute :name, kind_of: String, required: true, name_attribute: true
 attribute :canonical, kind_of: String, required: true
 attribute :view, kind_of: String
 
