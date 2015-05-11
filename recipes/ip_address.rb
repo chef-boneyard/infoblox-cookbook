da7a01921dddfc7c38d90803d237e3957912f24f
prabhu-das <prabhud@plumgrid.com>
2015-05-11 05:55:05 -0700

    Generalized some attributes for range.

--- recipes/ip_address.rb
+++ recipes/ip_address.rb
@@ -1,20 +1,13 @@
-
 infoblox_ip_address "Reserve static IP" do
   name node[:ip_address][:name]
+  usage_type node[:ip_address][:usage_type]
   ipv4addr node[:ip_address][:ipv4addr]
   action :reserve_static_ip
 end
 
-infoblox_ip_address "Reserve static IP" do
-  name node[:ip_address][:name]
-  usage_type 'dns'
-  ipv4addr node[:ip_address][:ipv4addr]
-  action :reserve_static_ip
-end
-
-infoblox_ip_address "Reserve static IP" do
+infoblox_ip_address "Reserve IP in Range" do
   name node[:ip_address][:ipv4addr]
-  usage_type 'fixed_address'
+  usage_type node[:ip_address][:usage_type]
   start_addr node[:ip_address][:start_addr]
   end_addr node[:ip_address][:end_addr]
   mac node[:ip_address][:mac]
