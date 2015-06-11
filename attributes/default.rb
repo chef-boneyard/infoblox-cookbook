ac34b48268cf2532c8acbb83f5a5ac601f706107
prabhu-das <prabhu.das@clogeny.com>
2015-06-11 17:49:01 +0530

    Updated some stuff.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # NIOS host details
-default['infoblox']['nios_appliance'] = ''
-default['infoblox']['username'] = 'admin'
-default['infoblox']['password'] = 'infoblox'
+default['infoblox']['nios_appliance'] = '' # ip address of the vNIOS appliance
+default['infoblox']['username'] = ''       # username of the vNIOS appliance
+default['infoblox']['password'] = ''       # password of the vNIOS appliance
 
 # vSphere vcenter server host details
 default['vcenter']['vcenter_host'] = ''
