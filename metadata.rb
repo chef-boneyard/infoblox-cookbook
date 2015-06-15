159f5ecce3efb7d061e788a82b2913b1d524a5ed
prabhu-das <prabhu.das@clogeny.com>
2015-06-15 18:20:22 +0530

    Updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'infoblox'
 maintainer       'Clogeny Technologies'
 maintainer_email 'clogeny.com'
 license          'All rights reserved'
-description      'Installs/Configures infoblox'
+description      'LWRP for automating vRealize Orchestrator workflows.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
